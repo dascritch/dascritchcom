d7021d570ae9896471142494dcf6493c350434a1
dascritch <xavier@dascritch.com>
2014-05-23 16:29:12 +0200

    Et c'est parti pour un rafraichissement

---
+++ README.md
@@ -0,0 +1,6 @@
+Site de présentation professionnel http://dascritch.com
+
+Refactorisation à partir de ce qui est actuellement hébergé par le service dagence et mis en place en 2011.
+
+La source des "news" vient de mon blog http://dascritch.net , les réalisations étaient éditées dans le service dagence
+
---
+++ aspects/une2014.js
@@ -0,0 +1,36 @@
+(function(document,$) {
+	"use strict";
+
+	if (document.all) {
+		// compatibilité MSIE 8 -, je garde car parfois, des clients sont taquins ou vraiment pas à jour
+	    var IEtristechose = ["section","header","article","aside","footer"];
+	    for (var i in IEtristechose) document.createElement(IEtristechose[i]);
+	}
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-81365-6']);
+	_gaq.push(['_trackPageview']);
+	var ga = document.createElement('script'); 
+	ga.type = 'text/javascript'; 
+	ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+
+	$(function(){
+		// obfuscation contre les malandrins . Évidemment, cela n'est pas accessible si javascript n'est pas là :(
+		document.getElementById('s2').innerHTML = '@';
+		document.getElementById('a4').href = 'mail'+'to:'+$('h2').text()+'?subject=J\'ai vu votre site professionnel';
+
+		var $carte = $('#carte');
+		// cela doit être faisable en css pur, à méditer
+		$carte.on('mouseenter focus','a',function() {
+			$carte.find('span').css('opacity',0.3);
+			$carte.find('span.'+this.className).css('opacity',1);
+		}).on('mouseleave blur','a',function() {
+			$carte.find('span').css('opacity',1);
+		});
+	});
+
+
+})(document,$);
