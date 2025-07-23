ad8dca8582db345a6e489eb5c3c35c595a7c997c
Xavier Mouton-Dubosc <xavier@dascritch.com>
2025-07-23 15:36:16 +0200

    removing jkéry

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ python3 data/parse.py
 scss -t compact aspects/source.scss aspects/compressed.css
 
 rm aspects/concatenated.js
-cat aspects/jquery2.js aspects/une2014.js > aspects/concatenated.js
+cp aspects/une2025.js aspects/concatenated.js
 cp aspects/concatenated.js aspects/compressed.js
 java -jar /usr/share/java/closure-compiler.jar --language_in ECMASCRIPT6 --compilation_level SIMPLE_OPTIMIZATIONS  --js aspects/concatenated.js --js_output_file aspects/compressed.js
 
