55517e7fad0c29e7956073575df63772cd264140
Xavier Mouton-Dubosc <xavier@dascritch.com>
2026-08-25 18:48:36 +0200

    remove vestigial build artifacts

--- make.sh
+++ make.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-wget -O data/webdev.html https://dascritch.net/category/Webdev
-wget -O data/self-business.html https://dascritch.net/tag/self-business
-python3 data/parse.py
+wget -O public/data/webdev.html https://dascritch.net/category/Webdev
+wget -O public/data/self-business.html https://dascritch.net/tag/self-business
+python3 parse.py
 
-scss -t compact aspects/source.scss aspects/compressed.css
 
-rm aspects/concatenated.js
-cp aspects/une2025.js aspects/concatenated.js
-cp aspects/concatenated.js aspects/compressed.js
-java -jar /usr/share/java/closure-compiler.jar --language_in ECMASCRIPT6 --compilation_level SIMPLE_OPTIMIZATIONS  --js aspects/concatenated.js --js_output_file aspects/compressed.js
-
-git add aspects/compressed.css aspects/concatenated.js aspects/compressed.js
+# scss -t compact aspects/source.scss aspects/compressed.css
+# 
+# rm aspects/concatenated.js
+# cp aspects/une2025.js aspects/concatenated.js
+# cp aspects/concatenated.js aspects/compressed.js
+# java -jar /usr/share/java/closure-compiler.jar --language_in ECMASCRIPT6 --compilation_level SIMPLE_OPTIMIZATIONS  --js aspects/concatenated.js --js_output_file aspects/compressed.js
+# 
+# git add aspects/compressed.css aspects/concatenated.js aspects/compressed.js
